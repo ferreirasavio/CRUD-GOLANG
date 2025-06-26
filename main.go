235ccfaf9098473765deb3d8624b24ad9b4c35e2
ferreirasavio <saviio.ferreiira15@gmail.com>
2025-06-25 22:44:56 -0300

    Refactor main function to use environment variable for port configuration

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"minha-api-go/database"
 	"minha-api-go/routes"
@@ -13,9 +14,14 @@ import (
 func main() {
 	database.Connect()
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3030"
+	}
+
 	r := mux.NewRouter()
 	routes.RegisterProductRoutes(r)
 
-	log.Println("Servidor rodando em http://localhost:3030")
-	log.Fatal(http.ListenAndServe(":3030", r))
+	log.Printf("Servidor rodando na porta %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
